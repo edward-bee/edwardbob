94b60c1748610560ba59aab5c9760c2cf5d6b1d6
edwxrdbobb <edwardbobkamara@gmail.com>
2024-01-26 00:10:35 +0000

    Your commit message here

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV !== "production",
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav)$/i,
+      use: {
+        loader: "url-loader",
+      },
+    });
+
+    return config;
+  },
+});
